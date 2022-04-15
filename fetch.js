fecf25f091ae4e0a47fe4089660f367bc086a65d
rummaniit <rummanoffice12@gmail.com>
2022-04-15 16:02:06 +0600

    2nd commit

--- fetch.js
+++ fetch.js
@@ -1,15 +1,37 @@
 let GetValue = () => {
     let GetValueFromInputBox = document.getElementById('input-city')
     let GetValueFromInputBoxText = GetValueFromInputBox.value
-    GetValueFromInputBox.value = ''
-    console.log(GetValueFromInputBoxText)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${GetValueFromInputBoxText}&appid=d9c43c8291eec5e2de752590fe76cef7`)
-        .then(res => res.json())
-        .then(data => disPlayTemp(data))
+    if (GetValueFromInputBoxText == '') {
+        let showDetails = document.getElementById('show-details')
+        let div = document.createElement('div')
+        div.classList.add('contain')
+        div.innerHTML = `
+       <h3>Please Enter any name</h3>`
+        showDetails.appendChild(div)
+    }
+
+    else {
+        GetValueFromInputBox.value = ''
+        console.log(GetValueFromInputBoxText)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${GetValueFromInputBoxText}&appid=d9c43c8291eec5e2de752590fe76cef7`)
+            .then(res => res.json())
+            .then(data => disPlayTemp(data))
+    }
 }
 let disPlayTemp = (tempa) => {
     console.log(tempa)
     let showDetails = document.getElementById('show-details')
+
+    if (!tempa.name) {
+        let showDetails = document.getElementById('show-details')
+        showDetails.textContent = ''
+        let div = document.createElement('div')
+        div.classList.add('contain')
+        div.innerHTML = `
+       <h3>Please Enter right name</h3>
+       `
+        showDetails.appendChild(div)
+    }
     if (tempa.main.temp > 300) {
         showDetails.textContent = ''
         let div = document.createElement('div')
@@ -40,5 +62,6 @@ let disPlayTemp = (tempa) => {
         `
         showDetails.appendChild(div)
     }
+
 
 }
